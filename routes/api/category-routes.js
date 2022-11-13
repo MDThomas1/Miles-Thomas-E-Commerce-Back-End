ad40a91fb751b3e2bdc149063f90221d7f067c5d
Miles Thomas <milesman2013@gmail.com>
2022-11-13 18:34:45 +1100

    delete routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,12 +36,37 @@ router.post('/', (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    })
+    if (!categoryData) {
+      res.status(404).json({ message: 'The category that you are searching for could not be found' })
+    } 
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'The category you wish to delete could not be found.' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
